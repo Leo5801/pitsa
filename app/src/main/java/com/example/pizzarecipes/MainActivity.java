b742fc380912d0e23d09ce1855e71ad3b0197174
Leo5801 <101037916+Leo5801@users.noreply.github.com>
2022-03-11 04:25:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pizzarecipes/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.pizzarecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<PizzaRecipeItem> pizzaRecipeItems = new ArrayList<>();
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_1_TITLE, Utils.PIZZA_1_DESCRIPTION,Utils.PIZZA_1_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_2_TITLE, Utils.PIZZA_2_DESCRIPTION,Utils.PIZZA_2_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_3_TITLE, Utils.PIZZA_3_DESCRIPTION,Utils.PIZZA_3_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_4_TITLE, Utils.PIZZA_4_DESCRIPTION,Utils.PIZZA_4_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_5_TITLE, Utils.PIZZA_5_DESCRIPTION,Utils.PIZZA_5_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_6_TITLE, Utils.PIZZA_6_DESCRIPTION,Utils.PIZZA_6_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_7_TITLE, Utils.PIZZA_7_DESCRIPTION,Utils.PIZZA_7_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_8_TITLE, Utils.PIZZA_8_DESCRIPTION,Utils.PIZZA_8_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_9_TITLE, Utils.PIZZA_9_DESCRIPTION,Utils.PIZZA_9_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,Utils.PIZZA_10_TITLE, Utils.PIZZA_10_DESCRIPTION,Utils.PIZZA_10_RECIPE));
+
+
+        recyclerView = findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+        adapter = new PizzaRescipeAdapter(pizzaRecipeItems,this);
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+}
